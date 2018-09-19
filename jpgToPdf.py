be020f37ac174c7261d6a5fa04a3e5245ad1851e
NatanMalta <natanmb13@gmail.com>
2018-09-19 13:57:59 -0300

    Script

--- jpgToPdf.py
+++ jpgToPdf.py
@@ -4,32 +4,26 @@ from PyPDF2 import PdfFileReader, PdfFileMerger
 
 
 nomePdf = str(input('Nome para salvar o pdf: '))
-todosArquivos = listdir()
-apenasJpg = []
-pdfs = []
+apenasJpg = [f for f in listdir() if f.lower().endswith("jpg")] # Lista apenas arquivos .jpg
+apenasJpg.sort()
+pdfs = [] 
 
-for indice in range(0, len(todosArquivos) ):
-    
-    if todosArquivos[indice].lower().endswith('.jpg'):
-        apenasJpg.append(todosArquivos[indice])# Adiciona apenas .jpg
-
-apenasJpg.sort() # Organiza a lista em ordem Alfanumérica
-
-for jpg in apenasJpg:
+for jpg in apenasJpg: 
     imagem = Image.open(jpg)
 
     if imagem.mode == "RGBA": # Converte imagens RGBA para RGB
         imagem = imagem.convert("RGB")
         
-    novoPdf = jpg.replace('.jpg', '.pdf')
-    pdfs.append(novoPdf)
+    novoPdf = jpg.replace('.jpg', '.pdf') # Substitui .jpg para .pdf
+    pdfs.append(novoPdf) # Adiciona na lista o nome do .pdf
 
-    if not path.exists(novoPdf):
+    if not path.exists(novoPdf): # Se não existir ele salva o .pdf
         imagem.save(novoPdf, "PDF", resolution=100.0)
 
 if not path.exists(nomePdf+'.pdf'):
     pdfs.sort() # Organiza a lista em ordem Alfanumérica
     merger = PdfFileMerger()
+    
     for pdf in pdfs:
         merger.append(PdfFileReader(path.join(pdf), 'rb'))
     merger.write(path.join(nomePdf+'.pdf'))
